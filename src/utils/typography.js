6d5fa25d594ed2a8a6a6e2a11f8704d5e60edfa0
Peter O'Connor <poc275@gmail.com>
2020-03-01 16:49:52 +0000

    Typography.js added.

---
+++ src/utils/typography.js
@@ -0,0 +1,9 @@
+import Typography from 'typography'
+import fairyGatesTheme from 'typography-theme-fairy-gates'
+
+// fairyGatesTheme.baseFontSize = '40px'
+
+const typography = new Typography(fairyGatesTheme)
+
+export const { scale, rhythm, options } = typography
+export default typography
