97e18582b07e50f99d67cad11ecd318932281326
Peter O'Connor <poc275@gmail.com>
2020-03-01 17:08:54 +0000

    Using Gatsby links for internal pages.

--- src/components/project.js
+++ src/components/project.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import Col from "react-bootstrap/Col"
 import styles from './project.module.scss'
+import { Link } from 'gatsby'
 
 export default (props) => (
     <Col md={3} className={styles.projectItem}>
         <div className={styles.projectItemOuter}>
-            <a href="/">
+            <Link to="/">
                 <div className={styles.projectItemInner} id={props.class}>
                     <div className={styles.overlay}>
                         <p className={styles.overlayTitle}>{props.title}</p>
                     </div>
                 </div>
-            </a>
+            </Link>
         </div>
     </Col>
 )
