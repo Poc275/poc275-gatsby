dd178383e0f6a7e46ba7ab55286259afdc5264c2
Peter O'Connor <poc275@gmail.com>
2020-03-06 18:08:54 +0000

    Menu and link styles added.

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,25 +1,28 @@
 import React from "react"
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
+import styles from './menu.module.scss'
 
 export default () => (
     <Navbar expand="md">
         <Navbar.Toggle aria-controls="navbar-content" />
-        <Navbar.Collapse id="navbar-content">
+        <Navbar.Collapse id="navbar-content" className="justify-content-center">
             <Nav>
-                <Nav.Link href="#about-me">
+                <Nav.Link href="#about-me" className={styles.menuLink}>
                     <span role="img" aria-label="who is this">👀 </span>who is this?
                 </Nav.Link>
-                <Nav.Link href="#my-work">
+                <Nav.Link href="#my-work" className={styles.menuLink}>
                     <span role="img" aria-label="my work">👨‍💻 </span>my work
                 </Nav.Link>
-                <Navbar.Text>
-                    Peter O'Connor
+                <Navbar.Text className={styles.menuName}>
+                    <Nav.Link href="/">
+                        Peter O'Connor
+                    </Nav.Link>
                 </Navbar.Text>
-                <Nav.Link href="https://app.swiftcv.com/@/poc275" target="_blank">
+                <Nav.Link href="https://app.swiftcv.com/@/poc275" target="_blank" className={styles.menuLink}>
                     <span role="img" aria-label="my CV">📝 </span>my CV
                 </Nav.Link>
-                <Nav.Link href="#contact">
+                <Nav.Link href="#contact" className={styles.menuLink}>
                     <span role="img" aria-label="who is this">📣 </span>contact
                 </Nav.Link>
             </Nav>
