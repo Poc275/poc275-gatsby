4657089a2fc1e39bd453d3301de5ebee29e8fb01
Peter O'Connor <poc275@gmail.com>
2020-03-28 10:31:59 +0000

    Added a project grid bg image frontmatter property.

--- src/components/project-grid.js
+++ src/components/project-grid.js
@@ -18,7 +18,7 @@ export default () => (
                             title
                             class
                             static
-                            featuredImage {
+                            projectBgImage {
                                 childImageSharp {
                                   fluid(maxWidth: 600) {
                                     ...GatsbyImageSharpFluid
@@ -39,7 +39,7 @@ export default () => (
                                  class={node.frontmatter.class}
                                  link={node.fields.slug}
                                  static={node.frontmatter.static}
-                                 bgImage={node.frontmatter.featuredImage.childImageSharp.fluid}
+                                 bgImage={node.frontmatter.projectBgImage.childImageSharp.fluid}
                         />
                     ))}
                 </Row>
