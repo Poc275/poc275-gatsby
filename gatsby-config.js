102da44fb53199c13b3cbf211e1ec90eebb49873
Peter O'Connor <poc275@gmail.com>
2020-03-31 17:18:01 +0100

    Fixed favicons not loading in production.

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,6 +55,51 @@ module.exports = {
         description: 'My personal programming portfolio',
         lang: 'en',
         icon: 'src/images/icon.png',
+        /* specifying icons manually due to a Apache/IIS bug where 
+          the default folder name 'icons' is aliased to the server's icons - 
+          https://github.com/gatsbyjs/gatsby/issues/12233 */
+        icons: [
+          {
+            "src": "favicons/icon-48x48.png",
+            "sizes": "48x48",
+            "type": "image/png"
+          },
+          {
+            "src": "favicons/icon-72x72.png",
+            "sizes": "72x72",
+            "type": "image/png"
+          },
+          {
+            "src": "favicons/icon-96x96.png",
+            "sizes": "96x96",
+            "type": "image/png"
+          },
+          {
+            "src": "favicons/icon-144x144.png",
+            "sizes": "144x144",
+            "type": "image/png"
+          },
+          {
+            "src": "favicons/icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "favicons/icon-256x256.png",
+            "sizes": "256x256",
+            "type": "image/png"
+          },
+          {
+            "src": "favicons/icon-384x384.png",
+            "sizes": "384x384",
+            "type": "image/png"
+          },
+          {
+            "src": "favicons/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          }
+        ],
         start_url: '/',
         display: 'standalone',
         background_color: "#fff",
