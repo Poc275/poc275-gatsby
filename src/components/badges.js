3afc3700994997d778f13cb598ca6df9da50da6e
Peter O'Connor <poc275@gmail.com>
2021-12-13 16:26:06 +0000

    Adds AZ204 badge.

--- src/components/badges.js
+++ src/components/badges.js
@@ -3,7 +3,8 @@ import Container from "react-bootstrap/Container"
 import Col from "react-bootstrap/Col"
 import Row from "react-bootstrap/Row"
 import styles from './badges.module.scss'
-import AzureBadge from '../images/microsoft-certified-azure-fundamentals.png'
+import Az900Badge from '../images/microsoft-certified-azure-fundamentals.png'
+import Az204Badge from '../images/microsoft-certified-azure-developer-associate.1.png'
 
 export default () => (
     <Container fluid={true} id="badges" className={styles.badges}>
@@ -11,7 +12,10 @@ export default () => (
             <Col className={styles.badgeCol}>
                 <h2><span role="img" aria-label="achievements">🏆</span></h2>
                 <a href="https://www.youracclaim.com/badges/8c738ab3-2984-4849-86e9-b060ce6dce74/public_url">
-                    <img src={AzureBadge} alt="azure fundamentals microsoft certified badge" className={styles.badgeImage} />
+                    <img src={Az900Badge} alt="azure fundamentals microsoft certified badge" className={styles.badgeImage} />
+                </a>
+                <a href="https://www.credly.com/badges/993e087f-3a53-48ae-8fb3-c71c1788a0eb/public_url">
+                    <img src={Az204Badge} alt="azure developer associate microsoft certified badge" className={styles.badgeImage} />
                 </a>
             </Col>
         </Row>
